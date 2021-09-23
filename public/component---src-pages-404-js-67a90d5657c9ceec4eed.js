a02b23f59c5e2f7c5b6a5b968aed5d5e2da5393a
szdenes <57998732+szdenes@users.noreply.github.com>
2021-09-23 15:59:37 +0200

    connection test to cpanel

---
+++ public/component---src-pages-404-js-67a90d5657c9ceec4eed.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_solidgarden_theme=self.webpackChunkgatsby_solidgarden_theme||[]).push([[883],{9616:function(e,t,n){n.r(t);var l=n(7294),a=n(3835);t.default=function(){return l.createElement(a.Ar,null,l.createElement(a.pQ,{title:"404: Not found",location:"/404"}),l.createElement("h1",null,"NOT FOUND"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-67a90d5657c9ceec4eed.js.map
